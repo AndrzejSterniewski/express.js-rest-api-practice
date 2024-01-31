20dfae22142e42c1ac2f87f8e4fbec9aa33e26a0
Andrzej Sterniewski <drapiszon@gmail.com>
2024-01-31 15:21:20 +0100

    split routes into separate files

---
+++ routes/seats.routes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+// SEATS
+
+router.route('/seats').get((req, res) => {
+    res.json(db.seats);
+});
+
+router.route('/seats/:id').get((req, res) => {
+    const item = db.seats.find(item => item.id === parseInt(req.params.id));
+    if (!item) res.json('item not found');
+    res.json(item);
+});
+
+router.route('/seats').post((req, res) => {
+    const item = {
+        id: uuidv4(),
+        day: req.body.day,
+        seat: req.body.seat,
+        client: req.body.client,
+        email: req.body.email
+    };
+    db.seats.push(item);
+    res.json({ message: 'OK' });
+});
+
+router.route('/seats/:id').put((req, res) => {
+    const item = db.seats.find(item => item.id === parseInt(req.params.id));
+
+    if (!item) res.json('item not found');
+
+    item.day = req.body.day;
+    item.seat = req.body.seat;
+    item.client = req.body.client;
+    item.email = req.body.email;
+
+    res.json({ message: 'OK' });
+});
+
+router.route('/seats/:id').delete((req, res) => {
+    const item = db.seats.find(item => item.id === parseInt(req.params.id));
+    if (!item) res.json('item not found');
+    const index = db.seats.indexOf(item);
+    db.seats.splice(index, 1);
+    res.json({ message: 'OK' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,68 +1,19 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const db = require('./db');
+// import routes
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
-
-// DONE
-app.get('/testimonials', (req, res) => {
-    res.json(db);
-});
-
-// DONE
-app.get('/testimonials/random', (req, res) => {
-    const random = db[Math.floor(Math.random() * db.length)];
-    res.json(random);
-});
-
-// DONE
-app.get('/testimonials/:id', (req, res) => {
-    // const itemId = parseInt(req.params.id);
-    // const item = db.find(item => item.id === itemId);
-    const item = db.find(item => item.id === parseInt(req.params.id));
-    if (!item) res.json('item not found');
-    res.json(item);
-});
-
-// DONE
-app.post('/testimonials', (req, res) => {
-    const item = {
-        id: uuidv4(),
-        author: req.body.author,
-        text: req.body.text
-    }
-    db.push(item);
-    res.json({ message: 'OK' });
-});
-
-// DONE
-app.put('/testimonials/:id', (req, res) => {
-    const item = db.find(item => item.id === parseInt(req.params.id));
-
-    if (!item) res.json('item not found');
-
-    item.author = req.body.author;
-    item.text = req.body.text;
-
-    res.json({ message: 'OK' });
-    // res.json(item);
-});
-
-// DONE
-app.delete('/testimonials/:id', (req, res) => {
-    const item = db.find(item => item.id === parseInt(req.params.id));
-    const index = db.indexOf(item);
-    db.splice(index, 1);
-    res.json({ message: 'OK' });
-});
+app.use('/', testimonialsRoutes);
+app.use('/', concertsRoutes);
+app.use('/', seatsRoutes);
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
