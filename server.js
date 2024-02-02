5d3c4e74df973c90c99b6bdb1b1e23d1b662de02
Andrzej Sterniewski <drapiszon@gmail.com>
2024-02-02 10:36:51 +0100

    added client files to project

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const db = require('./db');
+const cors = require('cors');
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -9,11 +10,14 @@ const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/', testimonialsRoutes);
-app.use('/', concertsRoutes);
-app.use('/', seatsRoutes);
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+// db.seats.some()
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
